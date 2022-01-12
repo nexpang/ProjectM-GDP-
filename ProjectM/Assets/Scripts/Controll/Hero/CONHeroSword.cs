2640fc02f8371eed5b05f5e15588c90d91ddb28c
Double_U_ <junhyup511@naver.com>
2022-01-12 11:09:18 +0900

    히어로 프리팹, 게임 UI

--- ProjectM/Assets/Scripts/Controll/Hero/CONHeroSword.cs
+++ ProjectM/Assets/Scripts/Controll/Hero/CONHeroSword.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CONHeroMan : CONHero
+public class CONHeroSword : CONHero
 {
     // 히어로 개별 유닛이 가지는 특성 구현
 }
